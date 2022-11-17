e1675559ec4314f43db9bcfb0b0d355168f759c7
FernandaSpineli <fernandaspinx@gmail.com>
2022-11-17 14:34:03 -0300

    criando entidade usuario e endereço

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    name = models.CharField(max_length=200)
+    cpf = models.CharField(max_length=14)
+    birth_date = models.DateField()
+    email = models.EmailField()
+    password = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20)
+    addresses = []
+    
+class Address(models.Model):
+    nickname = models.CharField(max_length=20)
+    street = models.CharField(max_length=30)
+    house_number = models.IntegerField()
+    zipcode = models.CharField(max_length=9)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=30)
+    
