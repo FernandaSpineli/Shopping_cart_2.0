7005b2e4318c2c8a8397b948c4f974ca2bdcbccc
FernandaSpineli <fernandaspinx@gmail.com>
2022-11-19 10:22:46 -0300

    criando rotas para endereço e alterando forma de importação

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from users.views import user_detail, user_list
+# from users.views import user_detail, user_list, address_detail, address_list
+from users import views
 
 urlpatterns = [
-    path('usuarios/', user_list),
-    path('usuarios/<int:id>/', user_detail)
+    path('usuarios/', views.user_list),
+    path('usuarios/<int:id>/', views.user_detail),
+    path('usuarios/<int:id>/addresses/', views.address_list),
+    path('usuarios/<int:id>/addresses/', views.address_detail)
 ]
