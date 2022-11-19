250cbb2be2c1387ca86424710960e037288afb49
FernandaSpineli <fernandaspinx@gmail.com>
2022-11-19 12:31:24 -0300

    criando metodos para endereços e alterando rotas

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,6 @@ from users import views
 urlpatterns = [
     path('usuarios/', views.user_list),
     path('usuarios/<int:id>/', views.user_detail),
-    path('usuarios/<int:id>/addresses/', views.address_list),
-    path('usuarios/<int:id>/addresses/', views.address_detail)
+    path('enderecos/', views.address_list),
+    path('enderecos/<int:id>/', views.address_detail)
 ]
--- users/views.py
+++ users/views.py
@@ -3,8 +3,8 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from users.models import User
-from users.serializers import UserSerializer
+from users.models import User, Address
+from users.serializers import UserSerializer, AddressSerializer
 
 @api_view(http_method_names=['GET', 'POST'])
 def user_list(request):
@@ -35,9 +35,31 @@ def user_detail(request, id):
             object.delete()
             return Response(status=204)
         
-    
-def address_list():
-    ...
+@api_view(http_method_names=['GET', 'POST'])    
+def address_list(request):
+    if request.method == 'GET':
+        objects = Address.objects.all()
+        serializer = AddressSerializer(objects, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    if request.method == 'POST':
+        serializer = AddressSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
 
-def address_detail():
-    ...
+@api_view(http_method_names=['GET', 'PATCH', 'DELETE'])
+def address_detail(request, id):
+    object = get_object_or_404(Address, id=id)
+    if request.method == 'GET':
+        serializer = AddressSerializer(object)
+        return JsonResponse(serializer.data)
+    if request.method == 'PATCH':
+        serializer = AddressSerializer(object, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=204)
+        return JsonResponse(serializer.errors, status=400)
+    if request.method == 'DELETE':
+            object.delete()
+            return Response(status=204)
