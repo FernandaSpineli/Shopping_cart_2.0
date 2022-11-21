a13d1df24b81edd5d7456a8cccd9d509ffb50a2e
FernandaSpineli <fernandaspinx@gmail.com>
2022-11-21 17:30:10 -0300

    estruturando testes e testando na classe business

--- users/business.py
+++ users/business.py
@@ -1,19 +1,15 @@
-
-
-password = 'Ab@123456'
-count = 0
-for p in password:
-    if (p.islower()): 
-        count+=1
-        print(f' {p}, letra minuscula {count}')
-    if (p.isupper()): 
-        count+=1
-        print(f' {p}, letra maiuscula {count}')
-    if(p=='@'or p=='$' or p=='_'): 
-        count+=1
-        print(f' {p}, simbolo {count}')
-if count >= 3: 
-    print("Valid Password") 
-else: 
-    print("Invalid Password") 
-
+address = [
+    {
+        'id': '1',
+        'rua': 'cinza'
+    },
+    {
+        'id': '2',
+        'rua': 'azul'
+    }
+]
+addresses = [
+    {'casa' : address[0]},
+    {'trabalho': address[1]}
+]
+print(addresses)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+import json
 
+from rest_framework.test import APITestCase
+
+from users.models import User
 # Create your tests here.
+
+class TestUsersList(APITestCase):
+    def test_user_created(self):
+        user_request_data = {
+            "name": "Carolina Santos",
+            "CPF": "12456789121",
+            "birth_date": "2001-3-01",
+            "email": "carolina32@gmail.com",
+            "password": "Aa@123548",
+            "phone_number": "37045674"
+        }
+        
+        response = self.client.post('/api/usuarios/', user_request_data, format='json')
+        
+        self.assertEqual(response.status_code, 201)
+        
+class TestUserDetail(APITestCase):
+    def test_get_user_by_id(self):
+        ...
+    def test_update_user(self):
+        ...   
+    def test_delete_user(self):
+        ...
+    
